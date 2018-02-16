a5772f0a7d7ac5649e2d40aecebca8430e98c10d
Michael Kapelko <kornerr@gmail.com>
2018-02-16 14:53:21 +0300

    Select S2/S3 display

---
+++ features/S1+PresentationSelection.swift
@@ -0,0 +1,31 @@
+FEATURE S1.swift/Impl
+
+private func displayScreen(name: String) {
+    let maybeVC = self.storyboard?.instantiateViewController(withIdentifier: name)
+    guard let vc = maybeVC else { return }
+    self.present(vc, animated: true, completion: nil)
+}
+
+@IBAction func displayS2(_ sender: Any) {
+    let sheet = UIAlertController(title: "Select", message: nil, preferredStyle: .actionSheet)
+    sheet.addAction(
+        UIAlertAction(
+            title: "Display S2",
+            style: .default,
+            handler: { [unowned self] _ in
+                self.displayScreen(name: "S2")
+            }
+        )
+    )
+    sheet.addAction(
+        UIAlertAction(
+            title: "Display S3",
+            style: .default,
+            handler: { [unowned self] _ in
+                self.displayScreen(name: "S3")
+            }
+        )
+    )
+    self.present(sheet, animated: true, completion: nil)
+}
+
--- versions/2/S1.swift
+++ versions/2/S1.swift
@@ -3,13 +3,38 @@ import UIKit
 
 class S1: UIViewController {
 
-    // S1+Presentation Start
-    @IBAction func displayS2(_ sender: Any) {
-        let maybeVC = self.storyboard?.instantiateViewController(withIdentifier: "S2")
+    // S1+PresentationSelection Start
+    
+    private func displayScreen(name: String) {
+        let maybeVC = self.storyboard?.instantiateViewController(withIdentifier: name)
         guard let vc = maybeVC else { return }
         self.present(vc, animated: true, completion: nil)
     }
-    // S1+Presentation End
+    
+    @IBAction func displayS2(_ sender: Any) {
+        let sheet = UIAlertController(title: "Select", message: nil, preferredStyle: .actionSheet)
+        sheet.addAction(
+            UIAlertAction(
+                title: "Display S2",
+                style: .default,
+                handler: { [unowned self] _ in
+                    self.displayScreen(name: "S2")
+                }
+            )
+        )
+        sheet.addAction(
+            UIAlertAction(
+                title: "Display S3",
+                style: .default,
+                handler: { [unowned self] _ in
+                    self.displayScreen(name: "S3")
+                }
+            )
+        )
+        self.present(sheet, animated: true, completion: nil)
+    }
+    
+    // S1+PresentationSelection End
 
 }
 
